ad185778b9e2161653056afc7b3aa895bf472a54
Pakhi Prerna <pakhi.prerna.cs27@iilm.edu>
2026-08-05 02:26:15 +0530

    Day 2 codes completed

---
+++ day2/work/LC414/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+
+        int count = 1;
+        int third = nums[nums.length - 1];
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+                third = nums[i];
+
+                if (count == 3) {
+                    return third;
+                }
+            }
+        }
+
+        return nums[nums.length - 1];
+    }
+}
---
+++ day2/work/LC485/Solution.java
@@ -0,0 +1,20 @@
+//max consecutive ones
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int count = 0;
+        int maxCount = 0;
+
+        for (int num : nums) {
+
+            if (num == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
